1d577625b816be8d00e7d6af6dde8999c14b94fe
billboz1@gmail.com <billboz1@gmail.com>
2014-11-04 09:11:57 -0500

    Adjusting for pg

--- db/migrate/20141104001410_setup_users.rb
+++ db/migrate/20141104001410_setup_users.rb
@@ -2,7 +2,7 @@ class SetupUsers < ActiveRecord::Migration
   def change
         user_id = User.create(name:'Bill Boz', age:'45')
 
-        Master.update_all "user = #{user_id}"
+        Master.update_all "user_id = #{user_id}"
 
   end
 end
