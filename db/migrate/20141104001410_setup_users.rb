f715a640cf75a15c924e875315a32f7e3e24a948
billboz1@gmail.com <billboz1@gmail.com>
2014-11-04 09:25:29 -0500

    another adjustment....
    note to self… this code is creating a sql statement and the string
    matters!

--- db/migrate/20141104001410_setup_users.rb
+++ db/migrate/20141104001410_setup_users.rb
@@ -1,8 +1,8 @@
 class SetupUsers < ActiveRecord::Migration
   def change
-        user_id = User.create(name:'Bill Boz', age:'45')
+        user = User.create(name:'Bill Boz', age:'45')
 
-        Master.update_all "user_id = #{user_id}"
+        Master.update_all "user_id = #{user.id}"
 
   end
 end
