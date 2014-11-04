039bdba2fe516cf7b2b486181b7af5dafccad662
billboz1@gmail.com <billboz1@gmail.com>
2014-11-04 09:05:23 -0500

    Added a user to migrate to pg without nil error

--- db/migrate/20141104001410_setup_users.rb
+++ db/migrate/20141104001410_setup_users.rb
@@ -1,6 +1,6 @@
 class SetupUsers < ActiveRecord::Migration
   def change
-        user_id = User.first
+        user_id = User.create(name:'Bill Boz', age:'45')
 
         Master.update_all "user = #{user_id}"
 
